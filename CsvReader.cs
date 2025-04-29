3ed50359b1bd4fd67ef74773145ba066a8021cec
Jan Leirvaag <lja022025@gmail.com>
2025-04-29 14:51:17 +0200

    update

--- CsvReader.cs
+++ CsvReader.cs
@@ -1,6 +1,19 @@
 public class CsvFileReader
 {
     static public string path = @"C:\Users\Instruktor\Desktop\CSharp\CSV_UFO\scrubbed.csv";
+
+    // static public DateTime ConvertToDateTime(UFO ufo[i],string stringDate)
+    // {
+    //     DateTime result;
+    //     //Parse string into DateTime type            
+    //     if (DateTime.TryParse(stringDate, out result))
+    //     {
+    //         ufo[i].Observed = result;
+    //         Console.WriteLine($"{ufo[i].Observed}");
+    //     }
+
+    //     return result;
+    // }
     static public void ReadFile()
     {
         //string path = @"C:\Users\Instruktor\Desktop\CSharp\CSV_UFO\scrubbed.csv";
@@ -21,26 +34,52 @@ public class CsvFileReader
         //Line 0 in lines are the headers
         for (int i = 1; i < lines.Length; i++)
         {
-            //DateTime dateValue;
+            DateTime dateValue;
 
             //Split each value pr/line based on field separator in file (in this case, a semicolon)
             string[] values = lines[i].Split(",");
 
-            //Parse string into DateTime type
-            //Something wrong happens here, using strings for the time being
-            // if (DateTime.TryParse(values[1], out dateValue))
-            //     ufo[i].Observed = dateValue;
+            //Parse string into DateTime type            
+            if (DateTime.TryParse(values[0], out dateValue))
+            {
+                ufo[i].Observed = dateValue;
+                Console.WriteLine($"{ufo[i].Observed}");
+            }
+
+            //ConvertToDateTime(ufo[i].Observed,values[0]);
 
             //Add each value into the corresponding object property
-            ufo[i].Observed = values[0];
+            //ufo[i].Observed = values[0];
             ufo[i].City = values[1];
             ufo[i].State = values[2];
             ufo[i].Country = values[3];
             ufo[i].Shape = values[4];
-            ufo[i].Duration_Seconds = values[5];
-            ufo[i].DurationHoursMinutes = values[6];
+
+            //Parse string into DateTime type            
+            if (DateTime.TryParse(values[5], out dateValue))
+            {
+                ufo[i].Duration_Seconds = dateValue;
+                Console.WriteLine($"{ufo[i].Duration_Seconds}");
+            }
+
+            //ufo[i].Duration_Seconds = values[5];
+            //ufo[i].DurationHoursMinutes = values[6];
+
+            if (DateTime.TryParse(values[6], out dateValue))
+            {
+                ufo[i].DurationHoursMinutes = dateValue;
+                Console.WriteLine($"{ufo[i].DurationHoursMinutes}");
+            }
+
             ufo[i].Comments = values[7];
-            ufo[i].DatePosted = values[8];
+            //ufo[i].DatePosted = values[8];
+
+            if (DateTime.TryParse(values[8], out dateValue))
+            {
+                ufo[i].DatePosted = dateValue;
+                Console.WriteLine($"{ufo[i].DatePosted}");
+            }
+
             ufo[i].Latitude = values[9];
             ufo[i].Longitude = values[10];
 
--- UFO.cs
+++ UFO.cs
@@ -1,15 +1,15 @@
 //Define UFO class. This will contain all info per UFO sighting
 public class UFO
 {
-    public string? Observed { get; set; }
+    public DateTime Observed { get; set; }
     public string? City { get; set; }
     public string? State { get; set; }
     public string? Country { get; set; }
     public string? Shape { get; set; }
-    public string? Duration_Seconds { get; set; }
-    public string? DurationHoursMinutes { get; set; }
+    public DateTime Duration_Seconds { get; set; }
+    public DateTime DurationHoursMinutes { get; set; }
     public string? Comments { get; set; }
-    public string? DatePosted { get; set; }
+    public DateTime DatePosted { get; set; }
     public string? Latitude { get; set; }
     public string? Longitude { get; set; }
 }
